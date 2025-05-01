30bfdbb17b40996b5e075167af2f176f8fe1e0e0
L3yu3D0r <LiYD13359710397@163.com>
2025-05-01 22:17:34 +0800

    Work Archiving 2025.05.01

--- src/UI/src/pages/Home/home.tsx
+++ src/UI/src/pages/Home/home.tsx
@@ -1,7 +1,9 @@
-// import { useState } from "preact/hooks";
-import preactLogo from "../../assets/preact.svg";
-// import { invoke } from "@tauri-apps/api/tauri";
-import { message } from "@tauri-apps/api/dialog"
+import zh_cnLP from "../../../public/zh-CN.json";
+import en_usLP from "../../../public/en-US.json";
+import { message } from "@tauri-apps/api/dialog";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./home.css";
+import { exit } from "@tauri-apps/api/process";
 
 export function Home() {
   // const [greetMsg, setGreetMsg] = useState("");
@@ -11,74 +13,66 @@ export function Home() {
   //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   //   setGreetMsg(await invoke("greet", { name }));
   // }
+  // By TongYiLingMa 2025.05.01
+  interface ConfigType {
+    language?: string;
+    [key: string]: any;
+  }
+
+  async function get_configs(): Promise<ConfigType> {
+    return JSON.parse(await invoke("get_configs"));
+  }
 
   function ECT(eca: number): string {
     switch (eca) {
       case 1:
         return ("code: 000001: Cannot Get The Element");
       case 2:
-        return ("code: 000002: Cannot Load Resources(Image)")
+        return ("code: 000002: Cannot Load Resources(Image)");
+      case 3:
+        return "code: 000003: Config File Error(In \"Language\")";
+      case 330:
+        return "code: 000330: 330 is Watching You";
+      case 330330:
+        return "code: 330330: 330 is Watching You Too";
+      default:
+        return "code: 999999: Unknown Error";
     };
-    return "Unknown Error";
   }
 
   async function throwError(ec: number) {
-    await message(ECT(ec), { title: "Error", type: "error" });
+    await message(ECT(ec).toString(), { title: "Error", type: "error" });
+    exit(1);
   }
 
-function init() {
-  var table = document.getElementById("main-table");
-  if (table) {
-
-  } else {
-    throwError(1);
+  function init() {
+    var configs: Promise<ConfigType> = get_configs();
+    configs.then(async (config) => {
+      if (config.language == "en-US") {
+        document.getElementById("toolbar-file")!.innerHTML = en_usLP.File;
+        document.getElementById("toolbar-edit")!.innerHTML = en_usLP.Edit;
+      } else if (config.language == "zh-CN") {
+        document.getElementById("toolbar-file")!.innerHTML = zh_cnLP.File;
+        document.getElementById("toolbar-edit")!.innerHTML = zh_cnLP.Edit;
+      } else {
+        throwError(3);
+      }
+    });
   }
-}
 
-return (
-  // <div class="container">
-  //   <h1>Welcome to Tauri!</h1>
-
-  //   <div class="row">
-  //     <a href="https://vitejs.dev" target="_blank">
-  //       <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-  //     </a>
-  //     <a href="https://tauri.app" target="_blank">
-  //       <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-  //     </a>
-  //     <a href="https://preactjs.com" target="_blank">
-  //       <img src={preactLogo} class="logo preact" alt="Preact logo" />
-  //     </a>
-  //   </div>
-
-  //   <p>Click on the Tauri, Vite, and Preact logos to learn more.</p>
-
-  //   <form
-  //     class="row"
-  //     onSubmit={(e) => {
-  //       e.preventDefault();
-  //       greet();
-  //     }}
-  //   >
-  //     <input
-  //       id="greet-input"
-  //       onInput={(e) => setName(e.currentTarget.value)}
-  //       placeholder="Enter a name..."
-  //     />
-  //     <button type="submit">Greet</button>
-  //   </form>
-
-  //   <p>{greetMsg}</p>
-  // </div>
-  <div onLoad={init}>
-    <table id={"main-table"}>
-      <tr id={"toolbar"}>
-        <td id={"toolbar-logo"}>
-          <img src={preactLogo} onError={() => throwError(2)} />
-        </td>
-        <td id={"toolbar-file"}>File</td>
-      </tr>
-    </table>
-  </div>
-);
+  return (
+    <div onLoad={init()}>
+      <div id={"toolbardiv"}>
+        <table id={"toolbar"}>
+          <tr>
+            <td id={"toolbar-logo"}>
+              <img src={"Logo.png"} onError={() => throwError(2)} />
+            </td>
+            <td id={"toolbar-file"}>{en_usLP.File}</td>
+            <td id={"toolbar-edit"}>{en_usLP.Edit}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
